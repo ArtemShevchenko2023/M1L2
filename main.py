0e6dee4de186a70d3305dce47f15da4f46726e5b
ArtemShevchenko2023 <129595378+ArtemShevchenko2023@users.noreply.github.com>
2023-04-10 00:18:37 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import random
-password = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+russian = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+english = "abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number = "1234567890"
+special = "~`\|.,+-/*!&$#?=@"
 vopros = int(input("Какая будет длина пароля?"))
 passw = input("Добавьте дополнительное слово к паролю если не хотите введите 0")
+russ = int(input("Сколько будет русских букв?"))
+eng = int(input("Сколько будет английских букв?"))
+num = int(input("Сколько будет цифр?"))
+spec = int(input("Сколько будет специальных символов?"))
 if passw != 0:
     print("Ваш пароль: ", passw, end="")
 else:
@@ -9,4 +16,4 @@ else:
 for i in range(vopros):
     pas = random.choice(password)
     print(pas, end="")
-print(end="\n")
+
