a49ec174ebd0d76b5b5bf2c53682d71f5c87b2f3
ArtemShevchenko2023 <129595378+ArtemShevchenko2023@users.noreply.github.com>
2023-04-09 23:09:40 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import random
-password = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+password = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
 vopros = int(input("Какая будет длина пароля?"))
-print("Ваш пароль: ", end="")
+passw = input("Добавьте дополнительное слово к паролю если не хотите введите 0")
+if passw != 0:
+    print("Ваш пароль: ", passw, end="")
+else:
+    print("Ваш пароль: ", end="")
 for i in range(vopros):
-    b = random.choice(password)
-    print(b, end="")
+    pas = random.choice(password)
+    print(pas, end="")
 print(end="\n")
